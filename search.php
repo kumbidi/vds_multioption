9b7b137a5d1e87bf545cd9ac2b726402d1f77a7d
orchidblues <34315930+orchidblues@users.noreply.github.com>
2020-10-05 16:20:00 +0530

    Add files via upload

--- search.php
+++ search.php
@@ -20,10 +20,16 @@
       data:'action=search&search_id='+search_id,
 
       success:function(msg){
+
         var ar=msg.split(",");
         $("#sear1").val(ar[0]);
-        $("#sear2").val(ar[1])	;
-        $("#sear3").val(ar[2])	;
+        $("#sear2").val(ar[1]);
+        $("#sear3").val(ar[2]);
+        $("#sear4").val(ar[3]);
+        $("#sear5").val(ar[4]);
+        $("#sear6").val(ar[5]);
+        $("#sear7").val(ar[6]);
+        $("#sear8").val(ar[7]);
         //window.location.reload();
 
       }
@@ -43,24 +49,62 @@
               <input type="text" name="search_id" id="search_id">
           </td>
           <td>
-          </td>
-      </tr>
-      <tr>
-          <td>
-              <input type="text" id="sear1" name="sear1">
-          </td>
-          <td>
-              <input type="text" id="sear2" name="sear2">
-          </td>
-          <td>
-              <input type="text" id="sear3" name="sear3">
-          </td>
-      </tr>
-      <tr>
-          <td>
               <input type="button" name="sub_id" value="Search" onclick="searchid()">
           </td>
       </tr>
+      <tr>
+          <td>Name: </td>
+          <td>
+              <input type="text" id="sear1" name="sear1">
+          </td>
+          <td></td>
+      </tr>
+      <tr>
+          <td>email: </td>
+          <td>
+              <input type="text" id="sear2" name="sear2">
+          </td>
+          <td></td>
+      </tr>
+      <tr>
+          <td>Mobile: </td>
+          <td>
+              <input type="text" id="sear3" name="sear3">
+          </td>
+          <td></td>
+      <tr>
+          <td>Amount: </td>
+          <td>
+              <input type="text" id="sear4" name="sear4">
+          </td>
+          <td></td>
+      </tr>
+      <tr>
+          <td>Details Of Pujas</td>
+          <td>
+              <input type="text" id="sear5" name="sear5">
+          </td>
+          <td></td>
+      </tr>
+      <tr>
+          <td></td>
+          <td>
+              <input type="text" id="sear6" name="sear6">
+          </td>
+      </tr>
+      <tr>
+          <td></td>
+          <td>
+              <input type="text" id="sear7" name="sear5">
+          </td>
+      </tr>
+      <tr>
+          <td></td>
+          <td>
+              <input type="text" id="sear8" name="sear6">
+          </td>
+      </tr>
+
 
     </table>
     </form>
--- updates.php
+++ updates.php
@@ -1,33 +1,54 @@
-<!-- <script src="js/jquery.min.js"></script> -->
 <?php
 	$servername="localhost";
   $username="root";
-  $password="root";
+  $password="";
 	$action=$_POST['action'];
 	$db="vds_puja";
         $conn=mysqli_connect($servername,$username,$password);
         if(!$conn)
         {
-        		die("Connection Failed :" .mysqli_connect_error());
-	}
-   	echo "Connected Successfully";
+        		die("Connection Failed :" .mysqli_connect_eerror());
+				}
+   	//echo "Connected Successfully";
 	mysqli_select_db($conn,$db);
 
   if($action=="register")
   {
-    $qryrateup=mysqli_query($conn,"insert into puja_details(date,time,seva,venue,activity,price) values('".$_POST['date_id']."','".$_POST['time_id']."','".$_POST['seva_id']."','".$_POST['venue_id']."','".$_POST['activity_id']."','".$_POST['price_id']."')");
+    $qryrateup=mysqli_query($conn,"insert into puja_details(Date,Time,Seva,Venue,Activity,Price) values('".$_POST['date_id']."','".$_POST['time_id']."','".$_POST['seva_id']."','".$_POST['venue_id']."','".$_POST['activity_id']."','".$_POST['price_id']."')");
     echo "Inserted";
     return;
   }
 
- elseif ($action=="search")
- {
-	$sear=mysqli_query($conn,"select * from puja_details where id='".$_POST['search_id']."'");
- 	//$num=mysqli_num_rows($qry);
-	$seardata=mysqli_fetch_array($sear);
-	echo $seardata['date'].",".$seardata['price'].",".$seardata['seva'];
-	return;
-}
+	elseif ($action=="search")
+	 {
+		 $search_id=$_POST['search_id'];
 
+		 $search_udetails=mysqli_query($conn,"select * from  user_details where uid='$search_id'");
+		 $search_data=mysqli_fetch_array($search_udetails);
+		 $uname=$search_data['name'];
+		 $uemail=$search_data['email'];
+		 $umobile=$search_data['mobile'];
+		 $uamount=$search_data['amount'];
+		 echo $uname. " , " .$uemail. " , " .$umobile. " , " .$uamount." ,";
 
+		 $search_sdetails=mysqli_query($conn,"select * from  sankalpa_details where user_id='$search_id'");
+
+		 $num=mysqli_num_rows($search_sdetails);
+		 for($i=0;$i<$num;$i++){
+			 $search_sdata=mysqli_fetch_array($search_sdetails);
+			 ${'san_details_' . $i}=$search_sdata['seva_id'].",";
+		 }
+		 for($i=0;$i<$num;$i++){
+			 $search_pujadetails=mysqli_query($conn,"select * from  puja_details where id='${'san_details_' . $i}'");
+			 $search_pujadata=mysqli_fetch_array($search_pujadetails);
+			 ${'puja_details_' . $i}=$search_pujadata['seva'].",";
+
+		 }
+
+		 for ($i=0;$i<$num;$i++) {
+		 	echo ${'puja_details_' . $i};
+		}
+		 return;
+
+	}
  ?>
